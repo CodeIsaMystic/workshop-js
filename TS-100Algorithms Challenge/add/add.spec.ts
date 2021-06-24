4eea7deb71f7ff8ed140a2bd73a253d53449e585
Julien Orjollet <oj.professionnel@gmail.com>
2021-06-24 11:45:02 +0200

    implemented add function with test suites

--- TS-100Algorithms Challenge/add/add.spec.ts
+++ TS-100Algorithms Challenge/add/add.spec.ts
@@ -1,44 +1,55 @@
-import { add, add2 } from './add';
+import { add1, add2 } from './add';
 
-xdescribe(add.name, () => {
-    it('test 1', () => {
-        // act
-        const result = add(1, 2);
 
-        // assert
-        expect(result).toBe(3);
-    });
 
-    it('test 2', () => {
-        // act
-        const result = add(3, 2);
 
-        // assert
-        expect(result).toBe(5);
-    });
-});
+describe(add1.name, () => {
+    test('add1 function is returned 3 as sum value', () => {
+        const expected = add1(1, 2);
 
-xdescribe(add2.name, () => {
-    // console.log(add2(2,3));
-    it('test 1', () => {
-        // arrange
+        expect(expected).toBe(3);
+    })
+})
+
+describe(add2.name, () => {
+    test('add2 function if it returns 3 as sum value', () => {
+        const expected = add2(1, 2);
+
+        expect(expected).toBe(3);
+    })
+})
+
+
+
+describe(add1.name, () => {
+    test('add1 function if it returns 3 the sum value with 1 and 2 as arguments', () => {
+        const expected = add1(1, 2);
+
+        expect(expected).toBe(3);
+    })
+
+    test('add1 function if it returns 5 the sum value with 3 and 2 as arguments', () => {
+        const expected = add1(3, 2);
+
+        expect(expected).toBe(5);
+    })
+})
+
+
+
+
+describe(add2.name, () => {
+    test('add2 function if it returns 15 of the data numbers array with values as arguments', () => {
         const data = [1, 2, 3, 4, 5];
+        const expected = add2(...data);
 
-        // act
-        const result = add2(...data);
+        expect(expected).toBe(15);
+    })
 
-        // assert
-        expect(result).toBe(15);
-    });
-
-    it('test 2', () => {
-        // arrange
+    test('add2 function if it returns 5 of the data numbers array with values as arguments', () => {
         const data = [2, 3];
+        const expected = add2(...data);
 
-        // act
-        const result = add2(...data);
-
-        // assert
-        expect(result).toBe(5);
-    });
-});
+        expect(expected).toBe(5);
+    })
+})
--- TS-100Algorithms Challenge/add/add.ts
+++ TS-100Algorithms Challenge/add/add.ts
@@ -1,12 +1,18 @@
-export function add(param1: number, param2: number): number {
-
+export function add1(number1: number, number2: number): number {
+  return number1 + number2
 }
 
-export function add2(...param1: number[]): number {
-
+export function add2(...sumNumbers: number[]): number {
+  return sumNumbers.reduce((previous, current) => {
+    return previous + current
+  })
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+
+/* add1  */
+console.log(add1(1, 2));
+console.log(add1(3, 2));
+
+/* add2  */
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
