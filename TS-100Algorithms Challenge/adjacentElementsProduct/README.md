529b554dd7b9c2bc90faccd7742f39d337c8a59f
Julien Orjollet <oj.professionnel@gmail.com>
2021-06-24 17:11:07 +0200

    test and implements adjacent elements products function

--- TS-100Algorithms Challenge/adjacentElementsProduct/README.md
+++ TS-100Algorithms Challenge/adjacentElementsProduct/README.md
@@ -1,5 +1,4 @@
-### Check Out My [YouTube Channel](https://www.YouTube.com/CodingTutorials360)
-
+### ADJACENT ELEMENTS PRODUCTS
 ---
 
 Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
--- TS-100Algorithms Challenge/adjacentElementsProduct/adjacentElementsProduct.spec.ts
+++ TS-100Algorithms Challenge/adjacentElementsProduct/adjacentElementsProduct.spec.ts
@@ -1,14 +1,17 @@
 import { adjacentElementsProduct } from './adjacentElementsProduct';
 
-xdescribe(adjacentElementsProduct.name, () => {
-    it('Test 1', () => {
-        // arrange
+describe(adjacentElementsProduct.name, () => {
+    test('in an array of integers and find the product of the two first pair indexes', () => {
+        const data = [3, 6];
+        const expected = adjacentElementsProduct(data);
+
+        expect(expected).toBe(18);
+    })
+
+    test('and find the largest pair product of the indexes', () => {
         const data = [3, 6, -2, -5, 7, 3];
+        const expected = adjacentElementsProduct(data);
 
-        // act
-        const response = adjacentElementsProduct(data);
-
-        // assert
-        expect(response).toBe(21);
-    });
-});
+        expect(expected).toBe(21);
+    })
+})
